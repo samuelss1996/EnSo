89f6a519cfbb10d56d96cc7b8561e1911a25796e
Orquídea <orquidea.seijas@gmail.com>
2017-04-20 14:26:03 +0200

    Actualizada consitencia MER-Diseño

--- anexos/ScriptBBDD.sql
+++ anexos/ScriptBBDD.sql
@@ -1,64 +1,74 @@
 CREATE DATABASE IF NOT EXISTS pruebasenso;
 
-CREATE TABLE if not exists centros (
+CREATE TABLE if not exists center (
   id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL
+  name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE if not exists usuarios(
-	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	idCentros INTEGER NOT NULL,
-	nombre VARCHAR(100) NOT NULL,
-	correoUsuario VARCHAR(50) NOT NULL,
-	tipoUsuario CHAR NOT NULL,
-  	FOREIGN KEY(idCentros) REFERENCES centros(id)
+CREATE TABLE if not exists user(
+	id CHAR(11) PRIMARY KEY,
+	idCenter INTEGER NOT NULL,
+	fistName VARCHAR(255) NOT NULL,
+	lastName VARCHAR(255) NOT NULL,
+	dateRegister DATE NOT NULL,
+	email VARCHAR(50),
+	nif CHAR(9) NOT NULL,
+	type CHAR(1),
+  	FOREIGN KEY(idCenter) REFERENCES center(id)
   		on delete restrict on update cascade
 );
 
-CREATE TABLE if not exists solicitudes(
+CREATE TABLE if not exists application(
 	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	estado VARCHAR(50) NOT NULL,
-	contenido TEXT NOT NULL
+	status VARCHAR(50) NOT NULL,
+	content TEXT NOT NULL
 );
 
-CREATE TABLE if not exists pertenecer(
-	idCentros INTEGER NOT NULL,
-	idUsuarios INTEGER NOT NULL,
-	idSolicitudes INTEGER NOT NULL,
-	PRIMARY KEY(idCentros, idUsuarios, idSolicitudes),
-  	FOREIGN KEY(idCentros) REFERENCES centros(id)
+CREATE TABLE if not exists belongTo(
+	idCenter INTEGER NOT NULL,
+	idUser CHAR(11) NOT NULL,
+	idApplication INTEGER NOT NULL,
+	PRIMARY KEY(idCenter, idUser, idApplication),
+  	FOREIGN KEY(idCenter) REFERENCES center(id)
   		on delete restrict on update cascade,  		
-  	FOREIGN KEY(idUsuarios) REFERENCES usuarios(id)
+  	FOREIGN KEY(idUser) REFERENCES user(id)
   		on delete restrict on update cascade,
-  	FOREIGN KEY(idSolicitudes) REFERENCES solicitudes(id)
+  	FOREIGN KEY(idApplication) REFERENCES application(id)
   		on delete restrict on update cascade
 );
 
-CREATE TABLE if not exists facturas(
-	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	precioTotal FLOAT NOT NULL,
-	fecha DATE NOT NULL,
-	idUsuarios INTEGER NOT NULL,
-	FOREIGN KEY(idUsuarios) REFERENCES usuarios(id)
+CREATE TABLE if not exists products(
+	id CHAR(11) NOT NULL PRIMARY KEY,
+	name VARCHAR(255),
+	stock INTEGER NOT NULL,
+	available BOOLEAN NOT NULL,
+	availableSince DATE NOT NULL,
+	category VARCHAR(255),
+	description TEXT(1024),
+	currentPrice FLOAT(4,2) NOT NULL
+);
+
+
+CREATE TABLE if not exists Sell(
+	id CHAR(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	totalPrice FLOAT(4,2) NOT NULL,
+	sellDate DATE NOT NULL,
+	idUser CHAR(11) NOT NULL,
+	FOREIGN KEY(idUser) REFERENCES user(id)
 		on delete restrict on update cascade
 );
 
-CREATE TABLE if not exists productos(
-	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	nombre VARCHAR(50) NOT NULL,
-	cantidad INTEGER NOT NULL,
-	disponible BOOLEAN NOT NULL
-);
 
 CREATE TABLE if not exists lineacompra(
-	cantidad INTEGER NOT NULL,
-	precioTotal FLOAT NOT NULL,
-	numLinea INTEGER NOT NULL,
-	idFacturas INTEGER NOT NULL,
-	idProductos INTEGER NOT NULL,
-	PRIMARY KEY(idFacturas, numLinea),
-	FOREIGN KEY(idFacturas) REFERENCES facturas(id)
+	quantity INTEGER NOT NULL,
+	totalPrice FLOAT(4,2) NOT NULL,
+	unitPrice FLOAT(4,2) NOT NULL,
+	lineSell INTEGER NOT NULL,
+	idSell CHAR(11) NOT NULL,
+	idProducts CHAR(11) NOT NULL,
+	PRIMARY KEY(idSell, numLinea),
+	FOREIGN KEY(idSell) REFERENCES Sell(id)
 		on delete restrict on update cascade,
-	FOREIGN KEY(idProductos) REFERENCES productos(id)
+	FOREIGN KEY(idProducts) REFERENCES products(id)
 		on delete restrict on update cascade
 );
