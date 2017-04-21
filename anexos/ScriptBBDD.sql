752bd7607c3d0b6dfa5f14b40bbc89a17c3f9d2f
Cristofer <cristofercnsdmngz@live.com>
2017-04-21 18:20:07 +0200

    Añadidos datos de prueba y fechas por defecto

--- anexos/ScriptBBDD.sql
+++ anexos/ScriptBBDD.sql
@@ -1,4 +1,8 @@
-CREATE DATABASE IF NOT EXISTS pruebasenso;
+CREATE USER 'enso'@'localhost' IDENTIFIED BY 'enso';
+CREATE DATABASE IF NOT EXISTS enso CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+GRANT USAGE ON *.* to 'enso'@'localhost' IDENTIFIED BY 'enso';
+GRANT ALL PRIVILEGES ON enso.* to 'enso'@'localhost';
+USE enso;
 
 CREATE TABLE if not exists center (
   id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -8,12 +12,12 @@ CREATE TABLE if not exists center (
 CREATE TABLE if not exists user(
 	id CHAR(11) PRIMARY KEY,
 	idCenter INTEGER NOT NULL,
-	fistName VARCHAR(255) NOT NULL,
+	firstName VARCHAR(255) NOT NULL,
 	lastName VARCHAR(255) NOT NULL,
-	dateRegister DATE NOT NULL,
+	dateRegister DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	email VARCHAR(50),
 	nif CHAR(9) NOT NULL,
-	type CHAR(1),
+	type CHAR(1), -- E, P, D
   	FOREIGN KEY(idCenter) REFERENCES center(id)
   		on delete restrict on update cascade
 );
@@ -52,7 +56,7 @@ CREATE TABLE if not exists products(
 CREATE TABLE if not exists Sell(
 	id CHAR(11) NOT NULL PRIMARY KEY,
 	totalPrice FLOAT(4,2) NOT NULL,
-	sellDate DATE NOT NULL,
+	sellDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	idUser CHAR(11) NOT NULL,
 	FOREIGN KEY(idUser) REFERENCES user(id)
 		on delete restrict on update cascade
@@ -72,3 +76,17 @@ CREATE TABLE if not exists lineacompra(
 	FOREIGN KEY(idProducts) REFERENCES products(id)
 		on delete restrict on update cascade
 );
+
+INSERT into center(name) values ('ETSE');
+
+INSERT into user(id, idCenter, firstName, lastName, email, nif, type) values ('U-AAAA-000', 1, 'Usuario1', 'Usuario', 'email@email.com', '12345678A', 'E');
+INSERT into user(id, idCenter, firstName, lastName, email, nif, type) values ('U-AAAA-001', 1, 'Usuario2', 'Usuario', 'email2@email.com', '09876543A', 'E');
+
+INSERT into application(status, content) values ('pending', 'Request for I-AAAA-000');
+
+INSERT into belongto(idCenter, idUser, idApplication) values (1, 'U-AAAA-000', 1);
+
+INSERT into products(id, name, stock, available, availableSince, category, description, currentPrice)
+       values ('I-AAAA-000', 'Producto1', '100', true, '2017-03-01', 'categoria', 'Descripcion del producto', 99.99);
+INSERT into products(id, name, stock, available, availableSince, category, description, currentPrice)
+       values ('I-AAAA-001', 'Producto2', '100', true, '2017-02-15', 'categoria', 'Descripcion del producto', 49.99);
