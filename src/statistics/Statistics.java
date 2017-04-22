7673b3eee6c99600f3fb224d7f445da7c794c5e1
Silvia <silvia.rodriguez.alcaraz@rai.usc.es>
2017-04-22 23:08:53 +0200

    Corrección clase Statistics

--- src/statistics/Statistics.java
+++ src/statistics/Statistics.java
@@ -3,96 +3,118 @@ package statistics;
 import model.DAOFactoryJDBC;
 import model.JDBCSellDAO;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Statistics implements IStatistics {
 	public int calculateLastWeekSellCount() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2017-04-17"));
-		from.setTime(Long.parseLong("2017-04-23"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -7);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateSellCount(to, from);
 	}
 
 	public int calculateLastMonthSellCount() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2017-03-01"));
-		from.setTime(Long.parseLong("2017-03-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -31);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateSellCount(to, from);
 	}
 
 	public int calculateLastYearSellCount() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2016-01-01"));
-		from.setTime(Long.parseLong("2016-12-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -365);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateSellCount(to, from);
 	}
 
 	public float calculateLastMonthDailyAverageSells() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2017-03-01"));
-		from.setTime(Long.parseLong("2017-03-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -31);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateDailyAverageSells(to, from);
 	}
 
 	public float calculateLastYearWeeklyAverageSells() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2016-01-01"));
-		from.setTime(Long.parseLong("2016-12-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -365);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateWeeklyAverageSells(to, from);
 	}
 
 	public int[] calculateLastMonthDailySellCount() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2017-03-01"));
-		from.setTime(Long.parseLong("2017-03-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -31);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateDailySellCount(to, from);
 	}
 
 	public int[] calculateLastYearWeeklySellCount() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2016-01-01"));
-		from.setTime(Long.parseLong("2016-12-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -365);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateWeeklySellCount(to, from);
 	}
 
 	public float[] calculateLastWeekDailyRelativeSells() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2017-04-17"));
-		from.setTime(Long.parseLong("2017-04-23"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -7);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateDailyRelativeSells(to, from);
 	}
 
 	public float[] calculateLastYearWeeklyRelativeSells() {
 		DAOFactoryJDBC factoryJDBC = new DAOFactoryJDBC();
 		JDBCSellDAO sellDAO = (JDBCSellDAO) factoryJDBC.getSellDAO();
-		Date to = new Date();
+
 		Date from = new Date();
-		to.setTime(Long.parseLong("2016-01-01"));
-		from.setTime(Long.parseLong("2016-12-31"));
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -365);
+		Date to = cal.getTime();
+
 		return sellDAO.calculateWeeklyRelativeSells(to, from);
 	}
 }
