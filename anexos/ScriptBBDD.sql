96c0371448b47d4848d53f6e541310f4e6610de3
Orquídea <orquidea.seijas@gmail.com>
2017-04-23 19:03:13 +0200

    Cambiado SQL (ver descripción)
    Se han cambiado las políticas de ON DELETE de restrict a CASCADE o SET NULL según correspondiese, de forma que se reduce el índice de petamientos.

--- anexos/ScriptBBDD.sql
+++ anexos/ScriptBBDD.sql
@@ -19,7 +19,7 @@ CREATE TABLE if not exists user(
 	nif CHAR(9) NOT NULL,
 	type CHAR(1), -- E, P, D
   	FOREIGN KEY(idCenter) REFERENCES center(id)
-  		on delete restrict on update cascade
+  		on delete set null on update cascade
 );
 
 CREATE TABLE if not exists application(
@@ -34,11 +34,11 @@ CREATE TABLE if not exists belongTo(
 	idApplication INTEGER NOT NULL,
 	PRIMARY KEY(idCenter, idUser, idApplication),
   	FOREIGN KEY(idCenter) REFERENCES center(id)
-  		on delete restrict on update cascade,  		
+  		on delete cascade on update cascade,  		
   	FOREIGN KEY(idUser) REFERENCES user(id)
-  		on delete restrict on update cascade,
+  		on delete cascade on update cascade,
   	FOREIGN KEY(idApplication) REFERENCES application(id)
-  		on delete restrict on update cascade
+  		on delete cascade on update cascade
 );
 
 CREATE TABLE if not exists products(
@@ -59,7 +59,7 @@ CREATE TABLE if not exists Sell(
 	sellDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	idUser CHAR(11) NOT NULL,
 	FOREIGN KEY(idUser) REFERENCES user(id)
-		on delete restrict on update cascade
+		on delete cascade on update cascade
 );
 
 
@@ -72,9 +72,9 @@ CREATE TABLE if not exists lineacompra(
 	idProducts CHAR(11) NOT NULL,
 	PRIMARY KEY(idSell, lineSell),
 	FOREIGN KEY(idSell) REFERENCES Sell(id)
-		on delete restrict on update cascade,
+		on delete cascade on update cascade,
 	FOREIGN KEY(idProducts) REFERENCES products(id)
-		on delete restrict on update cascade
+		on delete cascade on update cascade
 );
 
 INSERT into center(name) values ('ETSE');
